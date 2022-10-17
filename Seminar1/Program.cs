1ce6a6b3a97ad7cb9067a19566c2c06228723d71
Uladzimir <blind72@mail.ru>
2022-10-17 18:20:03 +0200

    Task 8 added

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -19,3 +19,33 @@ else
 }
 */
 
+/*
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+*/
+
+/*
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Write("Введите число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int num = 2;
+
+while(num <= N)
+{
+    Console.Write(num + " ");
+    num += 2;
+}
+*/
