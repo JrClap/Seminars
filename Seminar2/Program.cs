ea5d10585df3c2cd9826a61b1a644f87d3904d8b
Uladzimir <blind72@mail.ru>
2022-10-23 20:10:55 +0200

    Example 15 added

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -14,3 +14,39 @@ int result = numbers (num);
 
 Console.WriteLine($"New version of a numbers {num} number in the middle {result}");
 */
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+int numbers (int num)
+{
+    if(num > 99)
+    {
+
+    }
+}
+
+Console.Write("Введите положительное число - ");
+int num = Convert.ToInt32(Console.ReadLine());
+*/
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+bool day (int num)
+{
+    if(num == 6 || num == 7)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+Console.Write("Введите число обозначающее день недели - ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool result = day (num);
+
+Console.WriteLine($"Is the day of the week entered - {num} a weekday off? - {result}");
+*/
