35e479e12892b8b3857e1d1bddd19cf6cac6aa4f
Uladzimir <blind72@mail.ru>
2022-10-23 19:42:48 +0200

    Example 10 added

---
+++ Seminar2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+int numbers (int num)
+{
+    int a = num / 10;
+    int b = a % 10;
+    return b;
+}
+
+Console.Write("Введите любое 3-х значное число - ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = numbers (num);
+
+Console.WriteLine($"New version of a numbers {num} number in the middle {result}");
+*/
